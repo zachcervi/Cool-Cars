cae1ca188c5ba423ccbec1963b4aae03082e1fdd
Zach Cervi <zachcervi@gmail.com>
2020-03-20 15:15:55 -0600

    Selected Vehicle data is passed to second view

--- Cool Cars/Controller/MainVC.swift
+++ Cool Cars/Controller/MainVC.swift
@@ -11,8 +11,9 @@ import UIKit
 class MainVC: UIViewController {
 
     var selectedVehicle: Vehicle!
-    var challeneger = Vehicle.init(make: "Dodge", model: "Challenger", subModel: "SRT", year: "2020")
-    var jeep = Vehicle.init(make: "Jeep", model: "Wrangler", subModel: "Rubicon", year: "2020")
+    var challeneger = Vehicle.init(make: "Dodge", model: "Challenger", subModel: "SRT", year: "2020", mpg: "19/30", msrp: "38,995", hp: "350", zeroToSixty: "3.2")
+    
+    var jeep = Vehicle.init(make: "Jeep", model: "Wrangler", subModel: "Rubicon", year: "2020", mpg: "18/25", msrp: "27,343", hp: "285", zeroToSixty: "15")
     
     @IBOutlet weak var nextBtn: UIButton!
     
@@ -44,7 +45,8 @@ class MainVC: UIViewController {
     }
     
     @IBAction func unwindFromSelectedVehicleVC(undwindSegue: UIStoryboardSegue){
-        
+        selectedVehicle = nil
+        nextBtn.isEnabled = false
     }
 }
 
