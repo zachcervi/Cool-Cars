7bc97b1d7ea4cc75832e236330317248539f5f5a
Zach Cervi <zachcervi@gmail.com>
2020-03-20 14:36:33 -0600

    Segues to and from

--- Cool Cars/Controller/MainVC.swift
+++ Cool Cars/Controller/MainVC.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class MainVC: UIViewController {
 
+    var selectedVehicle: Vehicle!
+    var challeneger = Vehicle.init(make: "Dodge", model: "Challenger", subModel: "SRT", year: "2020")
+    var jeep = Vehicle.init(make: "Jeep", model: "Wrangler", subModel: "Rubicon", year: "2020")
+    
+    @IBOutlet weak var nextBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func onNextTapped(_ sender: Any) {
+        performSegue(withIdentifier: "ToSelectedVehicle", sender: self)
+    }
+    
+    @IBAction func onChallengerTapped(_ sender: Any) {
+        selectVehicle(vehicle: challeneger)
+    }
+    
+    @IBAction func onJeepTapped(_ sender: Any) {
+        selectVehicle(vehicle: jeep)
+    }
+    
+    func selectVehicle(vehicle: Vehicle){
+        selectedVehicle = vehicle
+        nextBtn.isEnabled = true
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let selectedVehicleVC = segue.destination as? SelectedVehicleVC {
+            selectedVehicleVC.selectedVehicle = selectedVehicle
+        }
+    }
+    
+    @IBAction func unwindFromSelectedVehicleVC(undwindSegue: UIStoryboardSegue){
+        
+    }
 }
 
---
+++ Cool Cars/Model/Vehicle.swift
@@ -0,0 +1,16 @@
+//
+//  Vehicle.swift
+//  Cool Cars
+//
+//  Created by Zach Cervi on 3/20/20.
+//  Copyright © 2020 Zach Cervi. All rights reserved.
+//
+
+import Foundation
+
+struct Vehicle{
+    var make: String!
+    var model: String!
+    var subModel: String!
+    var year: String!
+}
