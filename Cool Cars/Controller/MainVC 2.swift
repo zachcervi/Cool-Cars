89015d087994be56515bd203cde9f7476f81c3d0
Zach Cervi <zachcervi@gmail.com>
2020-03-20 15:27:20 -0600

    added screen shots

---
+++ Cool Cars/Controller/MainVC 2.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cool Cars
+//
+//  Created by Zach Cervi on 3/20/20.
+//  Copyright © 2020 Zach Cervi. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
